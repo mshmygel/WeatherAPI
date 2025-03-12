e6e58ff49f4575fab39fea3c9d66864128d294ae
Mike Shmygel <m.shmygel@gmail.com>
2025-03-12 12:17:54 +0200

    Updated Django admin panel with jazzmin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import WeatherData
 
-# Register your models here.
+@admin.register(WeatherData)
+class WeatherDataAdmin(admin.ModelAdmin):
+    list_display = ("city", "temperature", "humidity", "wind_speed", "description", "timestamp")
+    search_fields = ("city", "description")
+    list_filter = ("city", "timestamp")
+
