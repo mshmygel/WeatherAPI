04ab6a963d7b6c5fc8927df8842d1e83888cb273
Mike Shmygel <m.shmygel@gmail.com>
2025-03-12 12:18:08 +0200

    Updated API models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class WeatherData(models.Model):
+    city = models.CharField(max_length=100)
+    temperature = models.FloatField()
+    humidity = models.IntegerField()
+    wind_speed = models.FloatField()
+    description = models.CharField(max_length=255)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-timestamp"]
+
+    def __str__(self):
+        return f"{self.city} - {self.temperature}°C ({self.timestamp})"
+
