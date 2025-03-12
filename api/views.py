e51a06b684c97deb24d246ed02ecd37f9e282320
Mike Shmygel <m.shmygel@gmail.com>
2025-03-12 13:09:11 +0200

    Added filtering and sorting for weather API

--- api/views.py
+++ api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import viewsets, filters
+from .models import WeatherData
+from .serializers import WeatherDataSerializer
 
-# Create your views here.
+class WeatherDataViewSet(viewsets.ModelViewSet):
+    queryset = WeatherData.objects.all()
+    serializer_class = WeatherDataSerializer
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
+    filterset_fields = ["city", "temperature"]
+    ordering_fields = ["temperature", "humidity"]
