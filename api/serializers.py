db6ceff399abcd0665f3ed4306b977e0c7510cd8
Mike Shmygel <m.shmygel@gmail.com>
2025-03-13 10:38:46 +0200

    last commit

--- api/serializers.py
+++ api/serializers.py
@@ -5,3 +5,6 @@ class WeatherDataSerializer(serializers.ModelSerializer):
     class Meta:
         model = WeatherData
         fields = "__all__"
+
+class CitySerializer(serializers.Serializer):
+    city = serializers.CharField(required=True, max_length=100)
