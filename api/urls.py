b3e7b286eed9349552f1f4bb0eac30f79dd48ed5
Mike Shmygel <m.shmygel@gmail.com>
2025-03-13 10:36:40 +0200

    Add WeatherHistoryAPIView and configure routing for /api/history

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,19 @@
+# api/urls.py
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import WeatherDataViewSet, FetchWeatherAPIView, TaskStatusView
+from .views import (
+    WeatherDataViewSet,
+    FetchWeatherAPIView,
+    TaskStatusView,
+    WeatherHistoryAPIView
+)
 
 router = DefaultRouter()
 router.register(r'weather', WeatherDataViewSet)
 
 urlpatterns = [
     path("", include(router.urls)),
+
     path(
         "fetch-weather/",
         FetchWeatherAPIView.as_view(),
@@ -16,5 +23,10 @@ urlpatterns = [
         "task-status/<uuid:task_id>/",
         TaskStatusView.as_view(),
         name="task-status"
+    ),
+    path(
+        "history/",
+        WeatherHistoryAPIView.as_view(),
+        name="weather-history"
     ),
 ]
